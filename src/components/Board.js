d6a387bacd4547a168fddb981a4cdf4f58a32e73
Julian Nicholls <juliannicholls29@gmail.com>
2019-07-11 19:15:12 +0100

    Make selection work.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,46 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Cell from './Cell';
 
-const board = [
-  {
-    selected: false,
-    definite: 1,
-  },
-  {
-    selected: false,
-    possibles: '23',
-  },
-  {
-    selected: false,
-  },
-  {
-    selected: true,
-    set: 4,
-  },
-  {
-    selected: false,
-    possibles: '568923',
-  },
-  {
-    selected: false,
-  },
-  {
-    selected: false,
-    set: 7,
-  },
-  {
-    selected: false,
-  },
-  {
-    selected: false,
-  },
-];
+// const emptyBoard = new Array(81).fill({
+//   selected: false,
+//   definite: '',
+//   possibles: '',
+//   candidates: '',
+// });
 
 const Board = () => {
+  const [board, setBoard] = useState([]);
+
+  useEffect(() => {
+    const emptyBoard = [];
+
+    for (let i = 0; i < 81; ++i) {
+      emptyBoard[i] = {
+        selected: false,
+        definite: '',
+        possibles: '',
+        candidates: '',
+      };
+
+      setBoard(emptyBoard);
+    }
+  }, []);
+
   const clicked = e => {
+    const newBoard = board.slice(0);
     const index = Number(e.target.dataset.index);
-    board[index].selected = !board[index].selected;
+
+    // Unless the shift key is pressed, unselect all cells.
+    // Then set the clicked cell selected
+    if (!e.shiftKey) newBoard.forEach(cell => (cell.selected = false));
+
+    newBoard[index].selected = true;
+
+    setBoard(newBoard);
   };
 
   return (
