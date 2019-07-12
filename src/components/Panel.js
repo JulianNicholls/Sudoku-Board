04ab3ffd6a44bb738140ab57fa27722938431aa5
Julian Nicholls <juliannicholls29@gmail.com>
2019-07-12 20:45:24 +0100

    Panel with definites setting

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,17 +1,74 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { BoardContext } from '../context';
+
+const SET = 0;
+const POSSIBLES = 1;
+const CANDIDATES = 2;
 
 const Panel = () => {
+  const { setDefinite, setPossibles, setCandidates } = useContext(BoardContext);
+  const [mode, setMode] = useState(SET);
+
+  const setNumber = e => {
+    const value = e.target.innerText;
+    console.log({ value });
+
+    switch (mode) {
+      case SET:
+        setDefinite(e.target.innerText);
+        break;
+
+      case POSSIBLES:
+        setPossibles(e.target.innerText);
+        break;
+
+      case CANDIDATES:
+        setCandidates(e.target.innerText);
+        break;
+
+      default:
+        console.error('Huh?!');
+    }
+  };
+
   return (
     <div className="panel">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <button className="mode-button" onClick={() => setMode(SET)}>
+        Set
+      </button>
+      <button className="definite-button" onClick={setNumber}>
+        1
+      </button>
+      <button className="definite-button" onClick={setNumber}>
+        2
+      </button>
+      <button className="definite-button" onClick={setNumber}>
+        3
+      </button>
+      <button className="mode-button" onClick={() => setMode(POSSIBLES)}>
+        Corner
+      </button>
+      <button className="definite-button" onClick={setNumber}>
+        4
+      </button>
+      <button className="definite-button" onClick={setNumber}>
+        5
+      </button>
+      <button className="definite-button" onClick={setNumber}>
+        6
+      </button>
+      <button className="mode-button" onClick={() => setMode(CANDIDATES)}>
+        Middle
+      </button>
+      <button className="definite-button" onClick={setNumber}>
+        7
+      </button>
+      <button className="definite-button" onClick={setNumber}>
+        8
+      </button>
+      <button className="definite-button" onClick={setNumber}>
+        9
+      </button>
     </div>
   );
 };
--- src/context/index.js
+++ src/context/index.js
@@ -21,15 +21,56 @@ export const BoardProvider = ({ children }) => {
   }, []);
 
   const setSelected = (index, addToSelection) => {
-    console.log({ index, addToSelection });
-
     const newBoard = board.slice(0);
 
     // Clear the current selection unless adding to it.
     // Then set the selected cell
     if (!addToSelection) newBoard.forEach(cell => (cell.selected = false));
 
-    newBoard[index].selected = true;
+    newBoard[index].selected = !newBoard[index].selected;
+
+    setBoard(newBoard);
+  };
+
+  const getSelection = () => {
+    return board.reduce((indexes, cell, idx) => {
+      if (cell.selected) indexes.push(idx);
+
+      return indexes;
+    }, []);
+  };
+
+  const setDefinite = value => {
+    const selection = getSelection();
+    const newBoard = board.slice(0);
+
+    selection.forEach(idx => (newBoard[idx].definite = value));
+
+    setBoard(newBoard);
+  };
+
+  const setPossibles = value => {
+    value = String(value);
+
+    const selection = getSelection();
+    const newBoard = board.slice(0);
+
+    // selection.forEach(index => {
+    //   if(newBoard[index].possibles.include)
+    // });
+
+    setBoard(newBoard);
+  };
+
+  const setCandidates = value => {
+    value = String(value);
+
+    const selection = getSelection();
+    const newBoard = board.slice(0);
+
+    // selection.forEach(index => {
+    //   if(newBoard[index].possibles.include)
+    // });
 
     setBoard(newBoard);
   };
@@ -37,6 +78,9 @@ export const BoardProvider = ({ children }) => {
   const state = {
     board,
     setSelected,
+    setDefinite,
+    setPossibles,
+    setCandidates,
   };
 
   return <BoardContext.Provider value={state}>{children}</BoardContext.Provider>;
