590fa07fd6cec12add4866c124d2464591971494
Julian Nicholls <juliannicholls29@gmail.com>
2019-08-04 19:07:05 +0100

    Add SolvePage and SetPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Board from './components/Board';
 import Panel from './components/Panel';
 
 import { useBoard } from './context';
+
+const SetPage = () => {
+  return (
+    <>
+      <Board />
+      <Panel setMode={true} />
+    </>
+  );
+};
+
+const SolvePage = ({ match }) => {
+  const { loadBoard } = useBoard();
+
+  useEffect(() => {
+    console.log({ match, params: match.params, id: match.params.id });
+  }, [match]);
+
+  return (
+    <>
+      <Board />
+      <Panel />
+    </>
+  );
+};
 
 function App() {
   const { keyDownHandler } = useBoard();
@@ -13,10 +38,12 @@ function App() {
       <header className="App-header">
         <h1>Judoku</h1>
       </header>
-      <main>
-        <Board />
-        <Panel setMode={true} />
-      </main>
+      <Router>
+        <main>
+          <Route path="/" exact component={SetPage} />
+          <Route path="/board/:id" component={SolvePage} />
+        </main>
+      </Router>
     </div>
   );
 }
