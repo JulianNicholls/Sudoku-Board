096762a6d7ce566944e1fc3fef4698ce83a1962a
Julian Nicholls <juliannicholls29@gmail.com>
2020-05-22 21:10:35 +0100

    Redirect from /...id... to /board/...id...

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams,
+  Redirect,
+} from 'react-router-dom';
 
 import Board from './components/Board';
 import Panel from './components/Panel';
 
 import { useBoard } from './context';
 
-const SetPage = () => {
+const SetPage = () => (
+  <>
+    <Board />
+    <Panel setMode={true} />
+  </>
+);
+
+const SolvePage = () => {
+  const { id } = useParams();
+
   return (
     <>
-      <Board />
-      <Panel setMode={true} />
+      <Board id={id} />
+      <Panel />
     </>
   );
 };
 
-const SolvePage = ({ match }) => {
-  return (
-    <>
-      <Board id={match.params.id} />
-      <Panel />
-    </>
-  );
+const NotFound = () => {
+  const params = useParams();
+
+  if (params[0].length === 25)
+    return <Redirect to={{ pathname: `/board/${params[0].slice(1)}` }} />;
+
+  console.log({ params, p0: params[0] });
+
+  return <h2>Not Found</h2>;
 };
 
 function App() {
@@ -34,8 +51,19 @@ function App() {
       </header>
       <Router>
         <main>
-          <Route path="/" exact component={SetPage} />
-          <Route path="/board/:id" component={SolvePage} />
+          <Switch>
+            <Route exact path="/">
+              <SetPage />
+            </Route>
+
+            <Route path="/board/:id">
+              <SolvePage />
+            </Route>
+
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
         </main>
       </Router>
     </div>
